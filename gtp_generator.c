f83a457ac98393bab3e180b8ed1fd658d2804d72
Yi-Hua Wu <lsps4111wu@gmail.com>
2019-03-05 07:28:59 +0800

    Fix packet length error

--- gtp_generator.c
+++ gtp_generator.c
@@ -80,7 +80,6 @@ int main(int argc, char **argv)
 
 	gtpuheader->flag = 0x30;
 	gtpuheader->type = 255;
-	gtpuheader->length = htons(n+sizeof(gtp_header));
 	gtpuheader->teid = htonl(atol(argv[4]));
 	for(;;)
 	{
@@ -92,6 +91,7 @@ int main(int argc, char **argv)
 		// printf("Waiting for data...\n");
 		n = recvfrom(sockfd, recvbuffer,1600, 0, NULL, NULL);
 		printf("Received pkt size: %d\n",n);
+		gtpuheader->length = htons(n+sizeof(gtp_header));
 
 		struct ip* ip_header = (struct ip*) recvbuffer;
 		// printf("Before modification: %s\n",(inet_ntoa(ip_header->ip_dst)));
