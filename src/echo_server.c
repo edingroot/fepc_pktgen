8bbf1e128365e6c3f78afc29e88d9ee4c7005879
Chi Chang <edingroot@gmail.com>
2019-04-23 02:58:24 +0800

    Fix warnings

--- src/echo_server.c
+++ src/echo_server.c
@@ -1,28 +1,28 @@
-#include	<sys/types.h>
-#include	<sys/socket.h>
-#include	<netinet/in.h>
-#include	<arpa/inet.h>
-#include	<errno.h>
-#include	<fcntl.h>
-#include	<netdb.h>
-#include	<signal.h>
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<string.h>
-#include	<sys/stat.h>
-#include	<sys/uio.h>
-#include	<unistd.h>
-#include	<sys/wait.h>
-#include	<sys/un.h>
-#include	<ifaddrs.h>
-#include	<stdint.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/un.h>
+#include <ifaddrs.h>
+#include <stdint.h>
 
 // struct ip {
-// #if __BYTE_ORDER__ == LITTLE_ENDIAN 
+// #if __BYTE_ORDER__ == LITTLE_ENDIAN
 // 	u_char	ip_hl:4,		/* header length */
 // 		ip_v:4;			/* version */
 // #endif
-// #if __BYTE_ORDER__ == BIG_ENDIAN 
+// #if __BYTE_ORDER__ == BIG_ENDIAN
 // 	u_char	ip_v:4,			/* version */
 // 		ip_hl:4;		/* header length */
 // #endif
@@ -40,52 +40,51 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+	if (argc != 2)
 	{
 		printf("usage: echo_server <bind port>\n");
 		return -1;
 	}
 
-	int sockfd,n;
-	
-	struct sockaddr_in servaddr,remoteaddr;
-	char recvbuffer[1600],sendbuffer[2000];
+	int sockfd, n;
+
+	struct sockaddr_in servaddr, remoteaddr;
+	char recvbuffer[1600];
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	printf("sockfd = %d\n",sockfd);
+	printf("sockfd = %d\n", sockfd);
 
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family      = AF_INET;
+	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(atoi(argv[1]));
+	servaddr.sin_port = htons(atoi(argv[1]));
 
-	bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
+	bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
 	// remoteaddr.sin_family = AF_INET;
 	// remoteaddr.sin_port = htons(2152);
-	
+
 	// return 0;
-	int  remoteaddr_len = 0;
-    while(1){
+	int remoteaddr_len = 0;
+	while (1)
+	{
 		printf("Receiving data\n");
 
-		n = recvfrom(sockfd, recvbuffer,1600, 0, (struct sockaddr *)&remoteaddr, remoteaddr_len);
-		if(n == -1){
+		n = recvfrom(sockfd, recvbuffer, 1600, 0, (struct sockaddr *)&remoteaddr, (socklen_t *)(long)remoteaddr_len);
+		if (n == -1)
+		{
 			// printf("Received data %d\n",n);
-			printf("%d\n",errno);
+			printf("%d\n", errno);
 			perror("errno");
 			continue;
-			
 		}
 		// struct ip* ip_header = (struct ip*) recvbuffer;
 
-		printf("Received data %d\n",n);
+		printf("Received data %d\n", n);
 		recvbuffer[1599] = '\0';
 		printf("received: '%s' from client %s\n", recvbuffer,
-		       inet_ntoa(remoteaddr.sin_addr));
+			   inet_ntoa(remoteaddr.sin_addr));
 		// remoteaddr.sin_addr = ip_header->ip_src;
-		printf("Send data %d\n",n);
-		sendto(sockfd,recvbuffer,n,0,(struct sockaddr*)&remoteaddr,sizeof(remoteaddr));
-
+		printf("Send data %d\n", n);
+		sendto(sockfd, recvbuffer, n, 0, (struct sockaddr *)&remoteaddr, sizeof(remoteaddr));
 	}
-	
 }
